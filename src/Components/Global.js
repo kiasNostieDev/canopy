5ec6ff2cd7f71cffb0adc6a1a75109a2232f4dfc
Akinthiya-developer <akinthiyasrinath@gmail.com>
2020-08-15 16:10:28 +0530

    Global Dicussion Chat updation almost furnished

--- src/Components/Global.js
+++ src/Components/Global.js
@@ -1,19 +1,65 @@
-import React from 'react'
-import './Styles/Global.css'
+import React, { useState, useEffect } from "react";
+import "./Styles/Global.css";
+import { useRef } from "react";
 
 export default function Global() {
+  const [OpinionData, setOpinionData] = useState([]);
+  const currentOpinon = useRef();
+
+  function handleOpinionSend(event) {
+    const opinion = currentOpinon.current.value;
+    currentOpinon.current.value = null;
+    console.log(opinion);
+    setOpinionData((lastData) => {
+      return [...lastData, { name: "You", op: opinion }];
+    });
+  }
+
+  function handleEnter(event) {
+    console.log("holae");
+    if (event.keyCode === "13") handleOpinionSend();
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleEnter, false);
+  });
+
+  function SingleTonOpinion(props) {
     return (
-        <div className="Global">
-            <div className="GlobalTitle">GlobalDiscussion</div>
-            <div className="GlobalOpinion">
-                <div className="OpinionBox">
-                    <input placeholder="-->   Give out your opinions, ideas and information if you've got"></input>
-                    <div className="OpinionSend">
-                        <button className="OpinionButton">Send</button>
-                    </div>
-                </div>
+      <div>
+        {props.opinionLink.map((data) => {
+          return (
+            <div className="OpinionTile">
+              <div className="OpinionTileHead">{data.name}</div>
+              <div className="ActualOpinion">{data.op}</div>
             </div>
-            <div className="GlobalTalks"></div>
+          );
+        })}
+        {/* <div className="OpinionTileHead">{props.opinionLink.name}</div>
+            <div className="ActualOpinion">{props.opinionLink.op}</div> */}
+      </div>
+    );
+  }
+
+  return (
+    <div className="Global">
+      <div className="GlobalTitle">GlobalDiscussion</div>
+      <div className="GlobalOpinion">
+        <div className="OpinionBox">
+          <input
+            placeholder="-->   Give out your opinions, ideas and information if you've got"
+            ref={currentOpinon}
+          ></input>
+          <div className="OpinionSend">
+            <button className="OpinionButton" onClick={handleOpinionSend}>
+              Send
+            </button>
+          </div>
         </div>
-    )
+      </div>
+      <div className="GlobalTalks">
+        <SingleTonOpinion opinionLink={OpinionData} />
+      </div>
+    </div>
+  );
 }
